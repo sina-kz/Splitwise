a4701fea67ea84af4ca5b69ec1cbb2f5801798d3
nima <nimxj4141@gmail.com>
2021-07-30 14:16:28 +0430

    some models added

--- DongLand/admin.py
+++ DongLand/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
+from .models import Friend
+from .models import Bunch
+from .models import Expense
 
 UserAdmin.fieldsets += (
     ("Added Field", {'fields': ('phone_number', 'address')}),
@@ -8,3 +11,7 @@ UserAdmin.fieldsets += (
 
 # Register your models here.
 admin.site.register(User, UserAdmin)
+admin.site.register(Friend)
+admin.site.register(Bunch)
+admin.site.register(Expense)
+
--- DongLand/models.py
+++ DongLand/models.py
@@ -7,3 +7,28 @@ from django.db import models
 class User(AbstractUser):
     phone_number = models.CharField(max_length=100)
     address = models.CharField(max_length=200)
+
+
+class Friend(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False, related_name='friends')
+    friend = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False,
+                               related_name='knowns')
+
+
+class Bunch(models.Model):
+    users = models.ManyToManyField(User, null=False, blank=False, related_name="%(class)s_members")
+    name = models.CharField(max_length=150, unique=False, blank=False, null=False)
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False,
+                                related_name='%(class)s_creator')
+
+
+class Expense(models.Model):
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False,
+                                related_name='%(class)s_creator')
+    payers = models.ManyToManyField(User, blank=False, null=False, related_name="%(class)s_payers")
+    subject = models.CharField(max_length=150, blank=True, null=True, verbose_name='subject')
+    description = models.TextField(blank=True, unique=False, null=True, verbose_name='description')
+    location = models.TextField(blank=True, unique=False, null=True, verbose_name='location')
+    amount = models.FloatField(max_length=15, verbose_name="amount")
+    date_created = models.DateTimeField(auto_now_add=True, verbose_name='date')
+    picture = models.ImageField(upload_to="sina", blank=True, null=True, verbose_name='picture')
